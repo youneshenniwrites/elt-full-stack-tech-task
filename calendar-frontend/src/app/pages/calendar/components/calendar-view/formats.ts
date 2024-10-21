e5b401904107a42b249ffb4eb4e9c2b2cfe76ba4
Hugh Williams <hugh.williams@everylifetechnologies.com>
2024-10-21 18:06:09 +0100

    make gutter 24hr

--- calendar-frontend/src/app/pages/calendar/components/calendar-view/formats.ts
+++ calendar-frontend/src/app/pages/calendar/components/calendar-view/formats.ts
@@ -1,10 +1,19 @@
 import { Culture, DateLocalizer, DateRange, Formats } from 'react-big-calendar';
+import { localize24HrTime } from '../../calendar.utils';
 
 export const CalendarFormats: Formats = {
+  timeGutterFormat: (
+    date: Date,
+    culture?: Culture,
+    localizer?: DateLocalizer,
+  ) => {
+    return `${localize24HrTime(date, localizer)}`;
+  },
   eventTimeRangeFormat: (
     range: DateRange,
     culture?: Culture,
     localizer?: DateLocalizer,
-  ) =>
-    `${localizer?.format(range.start, 'HH:mm')} - ${localizer?.format(range.end, 'HH:mm')}`,
+  ) => {
+    return `${localize24HrTime(range.start, localizer)} - ${localize24HrTime(range.end, localizer)}`;
+  },
 };
